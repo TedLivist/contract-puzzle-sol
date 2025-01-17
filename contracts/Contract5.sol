939275424e74c954a2fa0bd7ca554060e2d8d21c
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-01-17 22:39:38 +0100

    Solve game test 5

--- contracts/Contract5.sol
+++ contracts/Contract5.sol
@@ -6,7 +6,7 @@ contract Game5 {
 
   address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
-  function win() external {
+  function win() external {    
     require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
 
     isWon = true;
--- test/game5Test.js
+++ test/game5Test.js
@@ -12,8 +12,30 @@ describe('Game5', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const signers = await ethers.getSigners();
+    
+    const threshold = BigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf");
+    
+    let wallet;
+    let attempts = 0;
+    do {
+      wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      attempts++;
+      if (attempts % 100 === 0) {
+        console.log(`Tried ${attempts} wallets...`);
+      }
+    } while (BigInt(wallet.address) >= threshold);
+    
+    console.log("Found valid wallet address:", wallet.address);
+    
+    const tx = await signers[1].sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1.0")
+    });
+    await tx.wait();
+    console.log("Funded wallet with 1 ETH");
 
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
